13128aadd331fc54e7fd75b6def0164a48d0c562
Gabriel George <56782229+ggeorge185@users.noreply.github.com>
2025-01-15 11:34:22 +0100

    Update Signup.jsx

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -57,7 +57,7 @@ const Signup = () => {
         <div className='flex items-center w-screen h-screen justify-center'>
             <form onSubmit={signupHandler} className='shadow-lg flex flex-col gap-5 p-8'>
                 <div className='my-4'>
-                    <h1 className='text-center font-bold text-xl'>LOGO</h1>
+                <img src="/image.png" alt="LOGO" className='text-center font-bold text-xl' style={{ width: '100px', height: 'auto' }} />
                     <p className='text-sm text-center'>Signup to see photos & videos from your friends</p>
                 </div>
                 <div>
