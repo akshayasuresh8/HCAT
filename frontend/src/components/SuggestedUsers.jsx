7de8bca08f87f03466966946ac4594651ffe9e77
Gabriel George <56782229+ggeorge185@users.noreply.github.com>
2025-01-16 03:42:34 +0100

    Update SuggestedUsers.jsx

--- frontend/src/components/SuggestedUsers.jsx
+++ frontend/src/components/SuggestedUsers.jsx
@@ -19,7 +19,9 @@ const SuggestedUsers = () => {
                                 <Link to={`/profile/${user?._id}`}>
                                     <Avatar>
                                         <AvatarImage src={user?.profilePicture} alt="post_image" />
-                                        <AvatarFallback>CN</AvatarFallback>
+                                        <AvatarFallback>
+  <img src="profile.png" alt="Fallback Profile" />
+</AvatarFallback>
                                     </Avatar>
                                 </Link>
                                 <div>
