905c1ee9ce207eb602bc1d40a5fb900ffecf4453
Gabriel George <56782229+ggeorge185@users.noreply.github.com>
2025-01-16 09:18:59 +0100

    Update RightSidebar.jsx

--- frontend/src/components/RightSidebar.jsx
+++ frontend/src/components/RightSidebar.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar'
 import { useSelector } from 'react-redux'
@@ -10,7 +9,7 @@ const RightSidebar = () => {
   return (
     <div className='w-fit my-10 pr-32'>
       <div className='flex items-center gap-2'>
-        <Link to={`/profile/${user?._id}`}>
+        <Link to={/profile/${user?._id}}>
           <Avatar>
             <AvatarImage src={user?.profilePicture} alt="post_image" />
             <AvatarFallback>
@@ -19,7 +18,7 @@ const RightSidebar = () => {
           </Avatar>
         </Link>
         <div>
-          <h1 className='font-semibold text-sm'><Link to={`/profile/${user?._id}`}>{user?.username}</Link></h1>
+          <h1 className='font-semibold text-sm'><Link to={/profile/${user?._id}}>{user?.username}</Link></h1>
           <span className='text-gray-600 text-sm'>{user?.bio || 'Bio here...'}</span>
         </div>
       </div>
