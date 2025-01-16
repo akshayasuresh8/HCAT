5029c12e8815e9dc4cc91a689aef8c6b0cd20035
Gabriel George <56782229+ggeorge185@users.noreply.github.com>
2025-01-16 09:30:36 +0100

    Update authSlice.js

--- frontend/src/redux/authSlice.js
+++ frontend/src/redux/authSlice.js
@@ -1,33 +1,48 @@
-import {createSlice} from "@reduxjs/toolkit"
+import { createSlice } from "@reduxjs/toolkit";
 
 const authSlice = createSlice({
-    name:"auth",
-    initialState:{
-        user:null,
-        suggestedUsers:[],
-        userProfile:null,
-        selectedUser:null,
+    name: "auth",
+    initialState: {
+        user: null,
+        suggestedUsers: [],
+        userProfile: null,
+        selectedUser: null,
     },
-    reducers:{
-        // actions
-        setAuthUser:(state,action) => {
+    reducers: {
+        setAuthUser: (state, action) => {
             state.user = action.payload;
         },
-        setSuggestedUsers:(state,action) => {
+        setSuggestedUsers: (state, action) => {
             state.suggestedUsers = action.payload;
         },
-        setUserProfile:(state,action) => {
+        setUserProfile: (state, action) => {
             state.userProfile = action.payload;
         },
-        setSelectedUser:(state,action) => {
+        setSelectedUser: (state, action) => {
             state.selectedUser = action.payload;
-        }
-    }
+        },
+        followUser: (state, action) => {
+            state.userProfile.followers.push(action.payload);
+            state.user.following.push(action.payload);
+        },
+        unfollowUser: (state, action) => {
+            state.userProfile.followers = state.userProfile.followers.filter(
+                (userId) => userId !== action.payload
+            );
+            state.user.following = state.user.following.filter(
+                (userId) => userId !== action.payload
+            );
+        },
+    },
 });
+
 export const {
-    setAuthUser, 
-    setSuggestedUsers, 
+    setAuthUser,
+    setSuggestedUsers,
     setUserProfile,
     setSelectedUser,
+    followUser,
+    unfollowUser,
 } = authSlice.actions;
+
 export default authSlice.reducer;
