8573c1b19484388a0dc3599ac640613d99abd438
Gabriel George <56782229+ggeorge185@users.noreply.github.com>
2025-01-20 00:52:42 +0100

    Update LeftSidebar.jsx

--- frontend/src/components/LeftSidebar.jsx
+++ frontend/src/components/LeftSidebar.jsx
@@ -50,7 +50,7 @@ const LeftSidebar = () => {
     const sidebarItems = [
         { icon: <Home />, text: "Home" },
         { icon: <Search />, text: "Search" },
-        { icon: <TrendingUp />, text: "Explore" },
+      //{ icon: <TrendingUp />, text: "Explore" },
         { icon: <MessageCircle />, text: "Messages" },
         { icon: <Heart />, text: "Notifications" },
         { icon: <PlusSquare />, text: "Create" },
