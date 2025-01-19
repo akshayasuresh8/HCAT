e85c1e8d645e512597f0879649f3e772aae747b7
Gabriel George <56782229+ggeorge185@users.noreply.github.com>
2025-01-19 20:44:29 +0100

    Update Posts.jsx

--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
-import Post from './Post'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import Post from './Post';
+import { useSelector } from 'react-redux';
 
 const Posts = () => {
-  const {posts} = useSelector(store=>store.post);
+  const { posts } = useSelector(store => store.post);
+
+  // Sort posts by creation time in descending order
+  const sortedPosts = [...posts].sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+
   return (
     <div>
-        {
-            posts.map((post) => <Post key={post._id} post={post}/>)
-        }
+      {sortedPosts.map((post) => <Post key={post._id} post={post} />)}
     </div>
-  )
-}
+  );
+};
 
-export default Posts
+export default Posts;
