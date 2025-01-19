f427718de2d6c352f6a81a09540ff2875a6c9c8d
Gabriel George <56782229+ggeorge185@users.noreply.github.com>
2025-01-20 00:54:32 +0100

    Update Messages.jsx

--- frontend/src/components/Messages.jsx
+++ frontend/src/components/Messages.jsx
@@ -15,10 +15,12 @@ const Messages = ({ selectedUser }) => {
         <div className='overflow-y-auto flex-1 p-4'>
             <div className='flex justify-center'>
                 <div className='flex flex-col items-center justify-center'>
-                    <Avatar className="h-20 w-20">
-                        <AvatarImage src={selectedUser?.profilePicture} alt='profile' />
-                        <AvatarFallback>CN</AvatarFallback>
-                    </Avatar>
+                    <Avatar>
+                                        <AvatarImage src={user?.profilePicture} alt="post_image" />
+                                        <AvatarFallback>
+  <img src="profile.png" alt="Fallback Profile" />
+</AvatarFallback>
+                                    </Avatar>
                     <span>{selectedUser?.username}</span>
                     <Link to={`/profile/${selectedUser?._id}`}><Button className="h-8 my-2" variant="secondary">View profile</Button></Link>
                 </div>
